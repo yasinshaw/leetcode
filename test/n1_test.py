cec7106c0ac08ab3bfb7b21b4526350f1b352bc7
yasinshaw <yasinshaw@gmail.com>
2020-02-16 11:53:11 +0800

    format n1_test.py

--- test/n1_test.py
+++ test/n1_test.py
@@ -2,13 +2,14 @@ import unittest
 
 from n1 import *
 
+solution = Solution()
+
 
 class MyTestCase(unittest.TestCase):
-    solution = Solution()
 
     def test_something(self):
-        self.assertEqual(self.solution.twoSum([1, 2, 3, 4], 5), [1, 2])
-        self.assertEqual(self.solution.twoSum([1, 3, 5, 6], 6), [0, 2])
+        self.assertEqual(solution.twoSum([1, 2, 3, 4], 5), [1, 2])
+        self.assertEqual(solution.twoSum([1, 3, 5, 6], 6), [0, 2])
 
 
 if __name__ == '__main__':
