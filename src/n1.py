4f0acfc46057f308f29d1c6e65dc6f4127a460e8
yasinshaw <yasinshaw@gmail.com>
2020-02-16 14:09:11 +0800

    refactor n1

--- src/n1.py
+++ src/n1.py
@@ -3,10 +3,10 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        number_map = {}
+        map = {}
         for index, num in enumerate(nums):
             another_num = target - num
-            if another_num in number_map:
-                return [number_map[another_num], index]
-            number_map[num] = index
+            if another_num in map:
+                return [map[another_num], index]
+            map[num] = index
         return []
