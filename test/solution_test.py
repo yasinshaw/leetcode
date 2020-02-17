fdec04b6d9cd601be29ed11bdf2b9cf52b0c9c55
yasinshaw <yasinshaw@gmail.com>
2020-02-17 22:23:54 +0800

    add solution.py

---
+++ test/solution_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from solution import *
+
+solution = Solution()
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_solution(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
