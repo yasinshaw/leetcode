317bb466c09ebc3b0c0c7ef5b7853cd87bbe7654
yasinshaw <yasinshaw@gmail.com>
2020-02-17 09:34:50 +0800

    implement n704

---
+++ src/n704.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + int((right - left) / 2)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
