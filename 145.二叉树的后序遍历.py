c0c1080891ef8acb5e6b363c8088f5c77926fcb7
yasinshaw <yasinshaw@gmail.com>
2020-09-11 07:48:50 +0800

    add 145

---
+++ 145.二叉树的后序遍历.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode.cn id=145 lang=python3
+#
+# [145] 二叉树的后序遍历
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        if not root:
+            return res
+        stack = []
+        cur = root
+        while cur or stack:
+            while cur:
+                res.append(cur.val)
+                stack.append(cur)
+                cur = cur.right
+            cur = stack.pop()
+            cur = cur.left
+        return res[::-1]
+        
+# @lc code=end
+
