7410a35d79fba96ba5e4738317300eb01afd0adb
yasinshaw <yasinshaw@gmail.com>
2020-09-13 08:04:50 +0800

    add 173

---
+++ 173.二叉搜索树迭代器.py
@@ -0,0 +1,51 @@
+#
+# @lc app=leetcode.cn id=173 lang=python3
+#
+# [173] 二叉搜索树迭代器
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.stack = []
+        self._push_left(root)
+
+
+    def _push_left(self, root: TreeNode):
+        while root:
+            self.stack.append(root)
+            root = root.left
+
+    
+    def next(self) -> int:
+        """
+        @return the next smallest number
+        """
+        cur = self.stack.pop()
+        if cur.right:
+            self._push_left(cur.right)
+        return cur.val
+
+
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
+        return len(self.stack) != 0
+
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
+# @lc code=end
+
