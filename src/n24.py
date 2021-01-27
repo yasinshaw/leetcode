8ef47029c0b3d7b159088cf37441219055b22dcb
qisu.xy <qisu.xy@alibaba-inc.com>
2021-01-27 12:53:55 +0800

    add n24

---
+++ src/n24.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=24 lang=python3
+#
+# [24] 两两交换链表中的节点
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# from common.Structure import ListNode
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        newHead = head.next
+        head.next = self.swapPairs(newHead.next)
+        newHead.next = head
+        return newHead
+
+
+# @lc code=end
+
