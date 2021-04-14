8430650c6ca74a68187e1b3480a32cf59dbea970
qisu.xy <qisu.xy@alibaba-inc.com>
2021-04-14 19:35:23 +0800

    update n69

--- src/n69.py
+++ src/n69.py
@@ -2,9 +2,12 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         if x < 2:
             return x
-        cur = x // 2
-        while True:
-            pre = cur
-            cur = (cur + x // cur) // 2
-            if cur * cur <= x < (cur + 1) * (cur + 1):
-                return cur
+        l, r,  res = 0, x, -1
+        while l <= r:
+            mid = (l + r) // 2
+            if mid * mid <= x:
+                res = mid
+                l = mid + 1 
+            else:
+                r = mid - 1
+        return res
