a3824a258e50c48cb8054c206df49e40413d4f3f
yasinshaw <yasinshaw@gmail.com>
2021-04-17 13:19:09 +0800

    add n78

--- src/n78.py
+++ src/n78.py
@@ -7,5 +7,15 @@
 # @lc code=start
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        # 回溯。定义一个递归函数，在[k, n - 1]找子集
+        def dfs(nums: List[int], path: List[int], result: List[List[int]], k: int):
+            result.append(path.copy())
+            for i in range(k, len(nums)):
+                newPath = path + [nums[i]]
+                dfs(nums, newPath, result, i + 1)
+                newPath.pop()
+        dfs(nums, [], result, 0)
+        return result
 # @lc code=end
 
