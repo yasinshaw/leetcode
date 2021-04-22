5ed36f8239f852cf8d709617717da422d3043d87
yasinshaw <yasinshaw@gmail.com>
2021-04-23 07:19:50 +0800

    add 2 medium

---
+++ src/n221.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=221 lang=python3
+#
+# [221] 最大正方形
+#
+
+# @lc code=start
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        # dp[i][j]是以matix[i][j]为右下角的正方形的最大边长
+        m = len(matrix)
+        n = len(matrix[0])
+        dp = [[0] * n for _ in range(m)]
+        maxSide = 0
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+                    maxSide = max(maxSide, dp[i][j])
+        return maxSide * maxSide
+
+
+
+# @lc code=end
+
---
+++ src/n322.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=322 lang=python3
+#
+# [322] 零钱兑换
+#
+
+# @lc code=start
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # 动态规划
+        dp = [sys.maxsize] * (amount + 1)
+        dp[0] = 0
+        for coin in coins:
+            for j in range(coin, amount + 1):
+                dp[j] = min(dp[j], dp[j - coin] + 1)
+
+        return dp[-1] if dp[-1] < sys.maxsize else -1
+
+# @lc code=end
+
