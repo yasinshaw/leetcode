3a3b7e735b33e0db62b0512735a6a083cdbb5bdd
yasinshaw <yasinshaw@gmail.com>
2021-04-28 07:44:47 +0800

    add n121

---
+++ src/n121.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=121 lang=python3
+#
+# [121] 买卖股票的最佳时机
+#
+
+# @lc code=start
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minPrice = prices[0]
+        maxProfit = 0
+        for price in prices:
+            minPrice = min(minPrice, price)
+            maxProfit = max(maxProfit, price - minPrice)
+        return maxProfit
+
+# @lc code=end
+
